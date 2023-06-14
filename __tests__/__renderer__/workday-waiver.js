cabe868de1af2dd949ea9155004e2edc96c1e3a9
정현모 <thesting1114@gmail.com>
2023-06-15 00:33:10 +0900

    Add describe and test (not delete waiver)
    Added a test function to solve the problem that rows are deleted depending on language selection when clicking the close button of the notification window created when the delete button is clicked

--- __tests__/__renderer__/workday-waiver.js
+++ __tests__/__renderer__/workday-waiver.js
@@ -212,6 +212,23 @@ describe('Test Workday Waiver Window', function()
         });
     });
 
+    describe('Not Delete waiver', () =>
+    {
+        test('Waiver was not deleted', () =>
+        {
+            prepareMockup();
+            addTestWaiver('2020-07-16', 'some reason');
+            const deleteBtn = document.querySelectorAll('#waiver-list-table .delete-btn')[0];
+            showDialog.mockImplementation((options, cb) =>
+            {
+                cb({ response: -1 });
+            });
+            deleteEntryOnClick({target: deleteBtn});
+            const length = document.querySelectorAll('#waiver-list-table .delete-btn').length;
+            expect(length).toBe(1);
+        });
+    });
+
     describe('Populating', () =>
     {
         const hd = new Holidays();
